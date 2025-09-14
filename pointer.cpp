de844a5f81611dd568943a8a9503dc7c4d618b2f
hts <2789428639@qq.com>
2025-09-14 09:35:01 +0800

    v1.1

--- pointer.cpp
+++ pointer.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int var =8;
-    void* ptr =&var;
-
-system("pause");
+    int* ptr =&var;
+    *ptr=10; //错误，void指针不能解引用
+    system("pause");
     return 0;
 }
